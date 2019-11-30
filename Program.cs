f9b799af78d163cf8951a89c915c1c174f6cdbc2
k0008451 <k0008451@C-HK-JYLEHT>
2019-11-30 14:16:02 +0200

    Lisätty testimielessä yksi rivi koodia

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ namespace Murresanasto
         static void Main(string[] args)
         {
             string path = @"C:\Users\k1901133\OneDrive - Epedu O365\Olio-ohjelmointi\Suomi-Etelä-Pohjanmaa.csv";
-            
+
+            //File.WriteAllText("testi.csv", "ekasolu;tokasolu");
 
             // murresanat
             Dictionary<string, string> murresana = new Dictionary<string, string>();
@@ -23,15 +24,16 @@ namespace Murresanasto
 
             int counter = 0;
             string line = "";
-            
+
 
             // Read the file and display it line by line.  
             System.IO.StreamReader file = new System.IO.StreamReader(path, System.Text.Encoding.GetEncoding("ISO-8859-1"));
             while ((line = file.ReadLine()) != null)
             {
-    
+
                 string[] sanat = line.Split(';');
-                if (!murresana.ContainsKey(sanat[0])) {
+                if (!murresana.ContainsKey(sanat[0]))
+                {
                     murresana.Add(sanat[0], sanat[1]);
                     Console.Write(sanat[0] + "\t" + sanat[1] + "\n");
                 }
